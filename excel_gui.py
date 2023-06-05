3c89930f20e61a64f41c5f8c3baad15615c87613
user-3135 <61242522+user-3135@users.noreply.github.com>
2023-06-05 18:10:52 -0500

    Update excel_gui.py

--- excel_gui.py
+++ excel_gui.py
@@ -4,13 +4,17 @@ from io import BytesIO
 import excel_file
 st.header('Upload Budget Comp')
 uploaded_file = st.file_uploader("Choose a file")
-dataframe_budget_comp = pd.read_excel(uploaded_file)
-output = BytesIO()
-prop_name = excel_file.create_excel(dataframe_budget_comp, output)
-name = prop_name + 'Budget Comp.xlsx'
-st.download_button(
-        label="Download Excel workbook",
-        data=output.getvalue(),
-        file_name=name,
-        mime="application/vnd.ms-excel"
-)
+
+try:
+        dataframe_budget_comp = pd.read_excel(uploaded_file)
+        output = BytesIO()
+        prop_name = excel_file.create_excel(dataframe_budget_comp, output)
+        name = prop_name + 'Budget Comp.xlsx'
+        st.download_button(
+                label="Download Excel workbook",
+                data=output.getvalue(),
+                file_name=name,
+                mime="application/vnd.ms-excel"
+        )
+except:
+        pass
