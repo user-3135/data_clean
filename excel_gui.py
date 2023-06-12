4b0cba119b5440debaae056650a25e0d8af4ee6b
user-3135 <61242522+user-3135@users.noreply.github.com>
2023-06-12 07:46:10 -0500

    Update excel_gui.py

--- excel_gui.py
+++ excel_gui.py
@@ -4,11 +4,30 @@ from io import BytesIO
 import excel_file
 st.header('Upload Budget Comp')
 uploaded_file = st.file_uploader("Choose a file")
-
+# income_statement_1
+st.header('Upload Income Statement')
+uploaded_file_income_statemnt_1 = st.file_uploader("Choose a file")
+# bal_sheet_1
+st.header('Upload Balance Sheet')
+uploaded_file_balance_sheet = st.file_uploader("Choose a file")
+# cash_flow_1_df
+st.header('Upload Statement of Cashflow')
+uploaded_file_cash_flow = st.file_uploader("Choose a file")
+# trail_balance_df
+st.header('Upload Trial Balance')
+uploaded_file_trial_balance = st.file_uploader("Choose a file")
+# payment_register_df
+st.header('Upload Payment Register')
+uploaded_file_payment_register = st.file_uploader("Choose a file")
 try:
         dataframe_budget_comp = pd.read_excel(uploaded_file)
+        dataframe_is = pd.read_excel(uploaded_file_income_statemnt_1)
+        dataframe_bs = pd.read_excel(uploaded_file_balance_sheet)
+        dataframe_cf = pd.read_excel(uploaded_file_cash_flow)
+        dataframe_tb = pd.read_excel(uploaded_file_trial_balance)
+        dataframe_pr = pd.read_excel(uploaded_file_payment_register)
         output = BytesIO()
-        prop_name = excel_file.create_excel(dataframe_budget_comp, output)
+        prop_name = excel_file.create_excel_2(dataframe_budget_comp, output, dataframe_is, dataframe_bs, dataframe_cf, dataframe_tb, dataframe_pr)
         name = prop_name + 'Budget Comp.xlsx'
         st.download_button(
                 label="Download Excel workbook",
