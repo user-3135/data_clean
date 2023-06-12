f38d3d8355f1e30a12c6e536c2246a9a28f8222c
user-3135 <61242522+user-3135@users.noreply.github.com>
2023-06-12 07:53:01 -0500

    Update excel_gui.py

--- excel_gui.py
+++ excel_gui.py
@@ -19,6 +19,7 @@ uploaded_file_trial_balance = st.file_uploader("Upload the Trial Balance")
 # payment_register_df
 st.header('Upload Payment Register')
 uploaded_file_payment_register = st.file_uploader("Upload teh Payment Register")
+dataframe_budget_comp = pd.read_excel(uploaded_file)
 dataframe_is = pd.read_excel(uploaded_file_income_statemnt_1)
 dataframe_bs = pd.read_excel(uploaded_file_balance_sheet)
 dataframe_cf = pd.read_excel(uploaded_file_cash_flow)
